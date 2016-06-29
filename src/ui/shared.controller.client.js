a20335f97799aba54f2f085bc1ecdcd020fc8027
coreyjbrown <coreyjbrown@gmail.com>
2016-06-29 19:11:44 -0400

    Add submit to form

--- src/ui/shared.controller.client.js
+++ src/ui/shared.controller.client.js
@@ -2,6 +2,12 @@ angular.module('MyApp', [])
 
 .controller('SharedController', function ($scope) {
 	var self = this;
+	self.attendee = {};
+
+	self.processScan = function(){
+		self.myAttendee = self.attendee.id;
+		self.attendee = {};
+	};
 	
 })
 ;
